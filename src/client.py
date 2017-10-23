889496fe0f3ed33bad1eb84cc26a8b4ae41e438c
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-23 14:42:04 -0700

    Changed port numbers to ints.

--- src/client.py
+++ src/client.py
@@ -2,6 +2,6 @@
 
 import socket
 
-client = socket.socket(*socket.getaddrinfo('127.0.0.1', '5000')[1][:3])
-client.connect(('127.0.0.1', '5000'))
+client = socket.socket(*socket.getaddrinfo('127.0.0.1', 5000)[1][:3])
+client.connect(('127.0.0.1', 5000))
 client.sendall('Hello from the client socket')
--- src/server.py
+++ src/server.py
@@ -7,7 +7,7 @@ import socket
 server = socket.socket(socket.AF_INET,
                        socket.SOCK_STREAM,
                        socket.IPPROTO_TCP)
-server.bind('127.0.0.1', '5000')
+server.bind(('127.0.0.1', 5000))
 server.listen(1)
 conn, addr = server.accept()
 conn.recv(8)
