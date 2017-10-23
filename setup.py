d82168799d4185ff309a93788ea3f22b7741767c
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-23 14:15:10 -0700

    Initial commit with setup, tox, client, server and test scripts.

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""Setup file for http-server package."""
+from setuptools import setup
+
+
+setup(
+    name="http-server",
+    description="Python http server package.",
+    author=["Michael Shinners", "Gabriel Meringolo"],
+    author_email=["michaelshinners@gmail.com", "gabriel.meringolo@gmail.com"],
+    license="MIT",
+    py_modules=["client", "server"],
+    package_dir={'': 'src'},
+    install_requires=[],
+    extras_require={
+        'testing': ['pytest', 'pytest-cov', 'pytest-watch', 'tox'],
+        'development': ['ipython']
+    },
+    entry_points={
+    }
+)
