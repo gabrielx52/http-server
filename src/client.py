b9c9a0dc7024ec0891b2f7bca8999d32e16dbea5
Michael Shinners <mishinners@icloud.com>
2017-10-23 14:32:56 -0700

    set up server.py and client.py

---
+++ src/client.py
@@ -0,0 +1,7 @@
+"""Client package for echo server."""
+
+import socket
+
+client = socket.socket(*socket.getaddrinfo('127.0.0.1', '5000')[1][:3])
+client.connect(('127.0.0.1', '5000'))
+client.sendall('Hello from the client socket')
---
+++ src/server.py
@@ -0,0 +1,13 @@
+"""Server package for echo server."""
+
+
+import socket
+
+
+server = socket.socket(socket.AF_INET,
+                       socket.SOCK_STREAM,
+                       socket.IPPROTO_TCP)
+server.bind('127.0.0.1', '5000')
+server.listen(1)
+conn, addr = server.accept()
+conn.recv(8)
