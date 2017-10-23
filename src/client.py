ea8becf21a371f2a5eea0b1e1231d661eba40a0b
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-23 15:29:21 -0700

    Created port vars for client and server, started server function.

--- src/client.py
+++ src/client.py
@@ -4,19 +4,21 @@ import socket
 
 
 def client(message):
-	"""Connect with server and send message."""
-    client = socket.socket(*socket.getaddrinfo('127.0.0.1', 5000)[1][:3])
-    client.connect(('127.0.0.1', 5000))
+    """Connect with server and send message."""
+    port = 5020
+    client = socket.socket(*socket.getaddrinfo('127.0.0.1', port)[1][:3])
+    client.connect(('127.0.0.1', port))
     client.sendall(message.encode('utf8'))
     buffer_length = 8
     reply_complete = False
     incoming_message = ''
     while not reply_complete:
-    	part = client.recv(buffer_length)
-    	incoming_message += part
-    	if len(part) < buffer_length:
-    		break
-    print(incoming_message.decode('utf8'))
+        part = client.recv(buffer_length)
+        incoming_message += part.decode('utf8')
+        if len(part) < buffer_length:
+            break
+    print(incoming_message)
+    client.close()
 
 
 
--- src/server.py
+++ src/server.py
@@ -1,13 +1,24 @@
 """Server package for echo server."""
 
-
 import socket
 
 
-server = socket.socket(socket.AF_INET,
-                       socket.SOCK_STREAM,
-                       socket.IPPROTO_TCP)
-server.bind(('127.0.0.1', 5000))
-server.listen(1)
-conn, addr = server.accept()
-conn.recv(8)
+def server():
+    """Start a server and echo all responses."""
+    port = 5020
+    server = socket.socket(socket.AF_INET,
+                           socket.SOCK_STREAM,
+                           socket.IPPROTO_TCP)
+    server.bind(('127.0.0.1', port))
+    server.listen(1)
+    conn, addr = server.accept()
+    buffer_length = 8
+    message_complete = False
+    incoming_message = ''
+    while not message_complete:
+        part = conn.recv(buffer_length)
+        incoming_message += part.decode('utf8')
+        if len(part) < buffer_length:
+            conn.sendall(incoming_message.encode('utf8'))
+            conn.close()
+            break
