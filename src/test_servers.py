aeaa46a06d459f00cdfc608128734fef634fb596
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-24 14:15:02 -0700

    Added tests to echo server. Added test to check buffer sized message and non-ascii characters for echo server.

--- src/test_servers.py
+++ src/test_servers.py
@@ -1,14 +1,11 @@
 """Test our Client-Server connection."""
-
-
-# import pytest
+# coding=utf-8
 
 
 def test_client():
     """Function tests that the server performs as planned."""
     from client import client
     reply = client('This is a test!')
-    print(reply)
     assert reply == 'This is a test!'
 
 
@@ -16,21 +13,27 @@ def test_client_message_is_shorter_than_buffer():
     """Function tests that the server performs as planned."""
     from client import client
     reply = client('a test')
-    print(reply)
     assert reply == 'a test'
 
 
 def test_client_message_is_much_longer_than_buffer():
     """Function tests that the server performs as planned."""
     from client import client
-    reply = client('This is a test of the emergency broadcast system. This is only a test.')
-    print(reply)
-    assert reply == 'This is a test of the emergency broadcast system. This is only a test.'
-"""
-The following conditions should be tested:
+    reply = client('This is a test of the emergency broadcast\
+ system. This is only a test.')
+    assert reply == 'This is a test of the emergency broadcast\
+ system. This is only a test.'
 
-messages shorter than one buffer in length
-messages longer than several buffers in length
-messages that are an exact multiple of one buffer in length
-messages containing non-ascii characters
-"""
+
+def test_client_message_same_as_buffer_length():
+    """Testing echo server with buffer sized message."""
+    from client import client
+    reply = client('12345678')
+    assert reply == '12345678'
+
+
+def test_non_ascii_characters_echo_server():
+    """Test echo server with non-ascii characters."""
+    from client import client
+    reply = client(u'Hüsker Dü')
+    assert reply == u'Hüsker Dü'
