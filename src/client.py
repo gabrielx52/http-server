62c2e5229020ddb04b0b5be613d90a0607e64ba7
Michael Shinners <mishinners@icloud.com>
2017-10-23 17:50:31 -0700

    Main func is operational. Need to solve clean exit name is main and tests.

--- src/client.py
+++ src/client.py
@@ -5,7 +5,7 @@ import socket
 
 def client(message):
     """Connect with server and send message."""
-    port = 5020
+    port = 5033
     client = socket.socket(*socket.getaddrinfo('127.0.0.1', port)[1][:3])
     client.connect(('127.0.0.1', port))
     client.sendall(message.encode('utf8'))
