0f7acd7e9066d7e768905a5baa406635edfc9335
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-25 14:26:42 -0700

    Updated README, added test. Updated the README to reflect additions to the package. Added test for server.response_ok(). Commented out old tests for echo server that no longer work after the refactor.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # http-server
 Code 401 Python http server package
+## Michael Shinners and Gabriel Meringolo
+
+
+### echo: Created an echo server that will echo back input messages.
+
+### step1: Added response ok and error functions. Will return 200 or 500 HTTP status code header responses.
--- src/test_servers.py
+++ src/test_servers.py
@@ -1,39 +1,45 @@
 """Test our Client-Server connection."""
 # coding=utf-8
 
-
-def test_client():
-    """Function tests that the server performs as planned."""
-    from client import client
-    reply = client('This is a test!')
-    assert reply == 'This is a test!'
-
-
-def test_client_message_is_shorter_than_buffer():
-    """Function tests that the server performs as planned."""
-    from client import client
-    reply = client('a test')
-    assert reply == 'a test'
+#Tests for echo server, will break with newer versions.
+# def test_client():
+#     """Function tests that the server performs as planned."""
+#     from client import client
+#     reply = client('This is a test!')
+#     assert reply == 'This is a test!'
 
 
-def test_client_message_is_much_longer_than_buffer():
-    """Function tests that the server performs as planned."""
-    from client import client
-    reply = client('This is a test of the emergency broadcast\
- system. This is only a test.')
-    assert reply == 'This is a test of the emergency broadcast\
- system. This is only a test.'
+# def test_client_message_is_shorter_than_buffer():
+#     """Function tests that the server performs as planned."""
+#     from client import client
+#     reply = client('a test')
+#     assert reply == 'a test'
 
 
-def test_client_message_same_as_buffer_length():
-    """Testing echo server with buffer sized message."""
-    from client import client
-    reply = client('12345678')
-    assert reply == '12345678'
+# def test_client_message_is_much_longer_than_buffer():
+#     """Function tests that the server performs as planned."""
+#     from client import client
+#     reply = client('This is a test of the emergency broadcast\
+#  system. This is only a test.')
+#     assert reply == 'This is a test of the emergency broadcast\
+#  system. This is only a test.'
 
 
-def test_non_ascii_characters_echo_server():
-    """Test echo server with non-ascii characters."""
-    from client import client
-    reply = client(u'Hüsker Dü')
-    assert reply == u'Hüsker Dü'
+# def test_client_message_same_as_buffer_length():
+#     """Testing echo server with buffer sized message."""
+#     from client import client
+#     reply = client('12345678')
+#     assert reply == '12345678'
+
+
+# def test_non_ascii_characters_echo_server():
+#     """Test echo server with non-ascii characters."""
+#     from client import client
+#     reply = client(u'Hüsker Dü')
+#     assert reply == u'Hüsker Dü'
+
+
+def test_response_ok():
+    """Test for server response_ok function."""
+    from server import response_ok
+    assert response_ok().endswith('\r\n\r\n')
