a395c6482fa6b0d4f1d218a6d2a0f76879491c24
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-25 13:45:40 -0700

    Changed encode/decode sequence. Changed the server-client encoding/decoding sequence to reduce errors. Sockets now close on KeyboardInterrupt.

--- src/client.py
+++ src/client.py
@@ -1,12 +1,12 @@
+# -*- coding: utf-8 -*-
 """Client package for echo server."""
-
 import socket
 import sys
 
 
 def client(message):
     """Connect with server and send message."""
-    port = 5045
+    port = 5059
     if len(message) % 8 == 0:
         message += ' '
     client = socket.socket(*socket.getaddrinfo('127.0.0.1', port)[1][:3])
@@ -14,15 +14,15 @@ def client(message):
     client.sendall(message.encode('utf8'))
     buffer_length = 8
     reply_complete = False
-    incoming_message = ''
+    incoming_message = b''
     while not reply_complete:
         part = client.recv(buffer_length)
-        incoming_message += part.decode('utf8')
+        incoming_message += part
         if len(part) < buffer_length:
             break
+    client.shutdown(socket.SHUT_WR)
     client.close()
-    print(incoming_message.strip())
-    return(incoming_message.strip())
+    print(incoming_message.decode('utf8'))
 
 
 if __name__ == "__main__":
--- src/server.py
+++ src/server.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 """Server package for echo server."""
+import sys
 import socket
 import email.utils
 
 
 def server():
     """Start a server and echo all responses."""
-    port = 5045
+    port = 5059
     server = socket.socket(socket.AF_INET,
                            socket.SOCK_STREAM,
                            socket.IPPROTO_TCP)
@@ -16,33 +18,36 @@ def server():
             conn, addr = server.accept()
             buffer_length = 8
             message_complete = False
-            incoming_message = ''
+            incoming_message = b''
             while not message_complete:
                 part = conn.recv(buffer_length)
-                incoming_message += part.decode('utf8')
+                incoming_message += part
                 if len(part) < buffer_length:
-                    response = response_ok()
-                    print(incoming_message)
-                    conn.sendall(response.encode('utf8'))
-                    conn.close()
-                    break
+                    message_complete = True
+            response = response_ok().encode('utf8')
+            conn.sendall(response)
+            print(incoming_message.decode('utf8'))
+            conn.close()
     except KeyboardInterrupt:
+        if conn:
+            conn.close()
         server.close()
+        sys.exit()
         print("\nGoodbye")
 
 
 def response_ok():
     """Set up socket and connection."""
     date_time = email.utils.formatdate(usegmt=True)
-    return "HTTP/1.1 200 OK<CRLF>\r\n" + "Date: " + date_time\
-           + "<CRLF>\r\n" + "<CRLF>\r\n"
+    return "HTTP/1.1 200 OK\r\n" + "Date: " + date_time\
+           + "\r\n\r\n"
 
 
 def response_error():
     """Response for 500 Internal Server Error."""
     date_time = email.utils.formatdate(usegmt=True)
-    return "HTTP/1.1 500 Internal Server Error<CRLF>\r\n" + "Date: "\
-           + date_time + "<CRLF>\r\n" + "<CRLF>\r\n"
+    return "HTTP/1.1 500 Internal Server Error\r\n" + "Date: "\
+           + date_time + "\r\n\r\n"
 
 
 if __name__ == "__main__":
