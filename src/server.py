93cfee94ebf59f1413033d3237634049da62c05f
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-26 14:43:32 -0700

    Refactored response error function. Refactored response error function to take params, will send back correct status codes.

--- src/server.py
+++ src/server.py
@@ -7,7 +7,7 @@ import email.utils
 
 def server():
     """Start a server and echo all responses."""
-    port = 5059
+    port = 5071
     server = socket.socket(socket.AF_INET,
                            socket.SOCK_STREAM,
                            socket.IPPROTO_TCP)
@@ -24,9 +24,9 @@ def server():
                 incoming_message += part
                 if b'@#FULL_STOP#@' in incoming_message:
                     message_complete = True
-            response = response_ok().encode('utf8')
-            conn.sendall(response)
-            print(incoming_message.decode('utf8'))
+            clean_request = incoming_message.replace(b'@#FULL_STOP#@', b'')
+            parsed = parse_request(clean_request)
+            conn.sendall(parsed)
             conn.close()
     except KeyboardInterrupt:
         if conn:
@@ -36,37 +36,40 @@ def server():
         print("\nGoodbye")
 
 
-def response_ok():
+def response_ok(uri):
     """Set up socket and connection."""
     date_time = email.utils.formatdate(usegmt=True)
-    return "HTTP/1.1 200 OK\r\n" + "Date: " + date_time\
-           + "\r\n\r\n"
+    return ("HTTP/1.1 200 OK\r\n" + "Date: " + date_time +
+            "\r\nResponse URI: {}\r\n\r\n").format(uri).encode('utf8')
 
 
 def response_error(code, phrase):
     """Response for 500 Internal Server Error."""
     date_time = email.utils.formatdate(usegmt=True)
     return ("HTTP/1.1 {} {}\r\nDate: " +
-            date_time + "\r\n\r\n").format(code, phrase)
+            date_time + "\r\n\r\n").format(code, phrase).encode('utf8')
 
 
 def parse_request(request):
     """Parse the client request to confirm validity."""
-    if request.endswith('\r\n\r\n'):
-        head, host, *_ = request.split("\r\n")
-        method, uri, protocol = head.split(' ')
-        host_url = host[6:]
-        if method != ("GET"):
-            raise TypeError("Your method request must be that of GET.")
-        if not host_url.startswith("www.") and not host.startswith("Host:"):
-            raise ValueError("Your requested url is not properly formatted or\
-                            could not be found.")
-        if protocol != ("HTTP/1.1"):
-            raise ValueError("Your request is not of the proper protocol.")
-        else:
-            return uri
+    if request.endswith(b'\r\n\r\n'):
+        try:
+            head, host, *_ = request.split(b"\r\n")
+            method, uri, protocol = head.split(b' ')
+            host_url = host[6:]
+            if method != (b"GET"):
+                return response_error("405", "Method not allowed.")
+            if not host_url.startswith(b"www.") and
+                                not host.startswith(b"Host:"):
+                return response_error("400", "Bad request.")
+            if protocol != (b"HTTP/1.1"):
+                return response_error("505", "HTTP version not supported.")
+            else:
+                return response_ok(uri.decode('utf8'))
+        except TypeError:
+            return response_error("400", "Bad request.")
     else:
-        raise ValueError("Your requested url is not properly formatted")
+        return response_error("400", "Bad request.")
 
 
 if __name__ == "__main__":
