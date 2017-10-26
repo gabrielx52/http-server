6cdf206a6aced783dee24ab9e10057239c3502cd
Michael Shinners <mishinners@icloud.com>
2017-10-26 15:34:16 -0700

    Step 2 testing complete. Completed all testing via tox. achieved 93% coverage. 11 passing tests.

--- src/server.py
+++ src/server.py
@@ -5,7 +5,7 @@ import socket
 import email.utils
 
 
-def server():
+def server():  # pragma: no cover
     """Start a server and echo all responses."""
     port = 5071
     server = socket.socket(socket.AF_INET,
@@ -54,13 +54,15 @@ def parse_request(request):
     """Parse the client request to confirm validity."""
     if request.endswith(b'\r\n\r\n'):
         try:
-            head, host, *_ = request.split(b"\r\n")
+            split_header = request.split(b"\r\n")
+            head, host = split_header[0], split_header[1]
             method, uri, protocol = head.split(b' ')
             host_url = host[6:]
             if method != (b"GET"):
                 return response_error("405", "Method not allowed.")
-            if not host_url.startswith(b"www.") and
-                                not host.startswith(b"Host:"):
+            if not host_url.startswith(b"www."):
+                return response_error("400", "Bad request.")
+            if not host.startswith(b"Host:"):
                 return response_error("400", "Bad request.")
             if protocol != (b"HTTP/1.1"):
                 return response_error("505", "HTTP version not supported.")
@@ -72,5 +74,5 @@ def parse_request(request):
         return response_error("400", "Bad request.")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     server()
--- src/test_servers.py
+++ src/test_servers.py
@@ -1,7 +1,7 @@
 """Test our Client-Server connection."""
 # coding=utf-8
 
-#Tests for echo server, will break with newer versions.
+# Tests for echo server, will break with newer versions.
 # def test_client():
 #     """Function tests that the server performs as planned."""
 #     from client import client
@@ -42,14 +42,14 @@
 def test_response_ok():
     """Test for server response_ok function."""
     from server import response_ok
-    assert response_ok().endswith('\r\n\r\n')
+    assert response_ok("uri").endswith(b'\r\n\r\n')
 
 
 def test_response_error():
     """Test that we get an 500 error message."""
     from server import response_error
     resp = response_error("500", 'Internal Server Error')
-    assert resp.startswith("HTTP/1.1 500")
+    assert resp.startswith(b"HTTP/1.1 500")
 
 
 def test_client_message_response_ok_end():
@@ -63,11 +63,67 @@ def test_client_message_response_ok_start():
     """Testing client response message start."""
     from client import client
     reply = client('test string')
-    assert reply.startswith('HTTP/1.1 200 OK')
+    assert reply.startswith('HTTP/1.1 400')
 
 
-def test_uri_response_from_parse_request():
+def test_parse_request_response_ok():
     """Check if URI is returned from parse request function."""
     from server import parse_request
-    req = "GET /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n"
-    assert parse_request(req) == "/index.html"
+    req = b"GET /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n"
+    assert parse_request(req).startswith(b'HTTP/1.1 200 OK')
+
+
+def test_parse_request_405_error():
+    """Check if URI is returned from parse request function."""
+    from server import parse_request
+    req = b"PUT /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n"
+    assert parse_request(req).startswith(b'HTTP/1.1 405')
+
+
+def test_parse_request_400_error_bad_url():
+    """Check if URI is returned from parse request function."""
+    from server import parse_request
+    req = b"GET /index.html HTTP/1.1\r\nHist: www.example.com\r\n\r\n"
+    assert parse_request(req).startswith(b'HTTP/1.1 400')
+
+
+def test_parse_request_400_error_bad_www():
+    """Check if URI is returned from parse request function."""
+    from server import parse_request
+    req = b"GET /index.html HTTP/1.1\r\nHost: wdw.example.com\r\n\r\n"
+    assert parse_request(req).startswith(b'HTTP/1.1 400')
+
+
+def test_parse_request_505_error_bad_protocol():
+    """Check if protocol is returned from parse request function."""
+    from server import parse_request
+    req = b"GET /index.html HTTP/2.1\r\nHost: www.example.com\r\n\r\n"
+    assert parse_request(req).startswith(b'HTTP/1.1 505')
+
+
+def test_parse_request_400_bad_request():
+    """Check if URI is returned from parse request function."""
+    from server import parse_request
+    req = b"GET /index.html HTTP/1.1\r\nHist: www.example.com\r\n"
+    assert parse_request(req).startswith(b'HTTP/1.1 400')
+
+
+def test_parse_request_400_error_bad_parse_request():
+    """Check if URI is returned from parse request function."""
+    from server import parse_request
+    req = b"GET /index.html HTTP/1.1\r\nHist: www.example.com\r\n\r\n"
+    assert parse_request(req).startswith(b'HTTP/1.1 400')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
